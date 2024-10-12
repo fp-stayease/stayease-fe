496d6e0f2e49303bd5d6ca15fda16a01d1fa0292
strwbry <annisamustofa@outlook.com>
2024-10-12 21:04:18 +0700

    feat(SE-62): enhance dashboard QuickActions grid for user and tenant roles
    Dashboard's QuickActions grid now displays relevant quick actions for both user and tenant roles

--- app/dashboard/(user)/UserDashboard.tsx
+++ app/dashboard/(user)/UserDashboard.tsx
@@ -3,13 +3,13 @@ import React from "react";
 import StatusGrid from "@/app/dashboard/(user)/_components/StatusGrid";
 import UpcomingTrips from "@/app/dashboard/(user)/_components/UpcomingTrips";
 import QuickActions from "@/app/dashboard/(user)/_components/QuickActions";
-import RecentActivity from "@/app/dashboard/(user)/_components/RecentActivity";
-import { CalendarDays, Home, MessageSquare, Star } from "lucide-react";
+import { CalendarDays, Home } from "lucide-react";
 import { useProfile } from "@/context/ProfileContext";
 import ProfileCard from "@/app/dashboard/(user)/_components/ProfileCard";
 import GlobalLoading from "@/components/GlobalLoading";
 import ErrorComponent from "@/components/ErrorComponent";
-import {useUserStats} from "@/hooks/reports/useUserStats";
+import { useUserStats } from "@/hooks/reports/useUserStats";
+import { userDashboardActions } from "@/constants/Routes";
 
 const UserDashboard: React.FC = () => {
   const { profile, isLoading, error } = useProfile();
@@ -26,25 +26,9 @@ const UserDashboard: React.FC = () => {
     return <ErrorComponent message={error.message} fullPage />;
   }
 
-  // !! TODO: Replace with real data
   const stats = [
     { icon: Home, label: "Upcoming Trips", value: userStats?.upcomingTrips },
-    { icon: CalendarDays, label: "Past Stays", value: userStats?.pastStays }
-  ];
-
-  const actions = [
-    {
-      icon: <Home className="h-4 w-4" />,
-      label: "Find a Place to Stay",
-    },
-    {
-      icon: <MessageSquare className="h-4 w-4" />,
-      label: "My Messages",
-    },
-    {
-      icon: <Star className="h-4 w-4" />,
-      label: "My Reviews",
-    },
+    { icon: CalendarDays, label: "Past Stays", value: userStats?.pastStays },
   ];
 
   return (
@@ -54,10 +38,16 @@ const UserDashboard: React.FC = () => {
       </h1>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
         <ProfileCard user={profile} className="col-span-1 md:col-span-3" />
-        <StatusGrid stats={stats} className="col-span-1 md:col-span-3" />
+        <StatusGrid
+          stats={stats}
+          className="col-span-1 md:col-span-3"
+          title="Travel Overview"
+        />
         <UpcomingTrips className="col-span-1 md:col-span-2" />
-        <QuickActions actions={actions} className="col-span-1 md:col-span-2" />
-        <RecentActivity className="col-span-1 md:col-span-3" />
+        <QuickActions
+          actions={userDashboardActions}
+          className="col-span-1 md:col-span-2"
+        />
       </div>
     </div>
   );
